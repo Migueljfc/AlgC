b15b5ee1c87efab74939a94bf868fcea253f7902
Migueljfc <miguel.f.cabral@gmail.com>
2020-03-11 14:59:39 +0000

    aula3 completa

--- aula3/ex2.c
+++ aula3/ex2.c
@@ -25,9 +25,24 @@ int findGreatest(int array[],int n){
 }
 
 int main(void){
-    int array[10]  =   {2,2,2,2,2,2,2,2,2,10};
+    int array[10]  =  {1,  9,  2,  8,  3,  4,  5,  3,  7,  2};
     size_t n = sizeof(array)/sizeof(int);
     int indice = findGreatest(array,n);
-    printf("%d - %d \n",indice,nComp);
-    
+    printf("%d - %d\n",indice,nComp);
+    printf("_________________________\n");
+    nComp = 0;
+    int array2[10]  =  {1,  7,  4,  6,  5,  2,  3,  2,  1,  0};
+    indice = findGreatest(array2,n);
+    printf("%d - %d\n",indice,nComp);
+    printf("_________________________\n");
+    nComp = 0;
+    int array3[10]  =  {2,2,2,2,2,2,2,2,2,2};
+    indice = findGreatest(array3,n);
+    printf("%d - %d\n",indice,nComp);
+     printf("_________________________\n");
+    nComp = 0;
+    int array4[5]  =  {2,2,2,2,5};
+    n = sizeof(array4)/sizeof(int);
+    indice = findGreatest(array4,n);
+    printf("%d - %d\n",indice,nComp);
 }
